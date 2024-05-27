ef8176941a470ed0c346794fe1ad43d64a300bf3
Alles Abinash <allesabinash1@gmail.com>
2024-05-27 16:28:34 +0530

    part23

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -67,11 +67,17 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                if (context.mounted) {
+
+                final user = FirebaseAuth.instance.currentUser;
+                print(user);
+                if (user?.emailVerified ?? false) {
+                  // user email is verified
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                } else {
+                  // user email is not verified
                   Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
+                      verifyEmailRoute, (route) => false);
                 }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
